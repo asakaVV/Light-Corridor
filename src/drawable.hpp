c1fe22bc7f9df219a412b650ff69eea40f88d5d6
Romain FORBIN <romain@macbook-pro-de-romain.home>
2024-05-03 22:13:58 +0200

    add the if apple condition in drawable

--- src/drawable.hpp
+++ src/drawable.hpp
@@ -2,7 +2,11 @@
 
 #define GL_SILENCE_DEPRECATION
 #include "../third_party/glfw/include/GLFW/glfw3.h"
+#ifdef __APPLE__
 #include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
 #include "3D_tools.hpp"
 
 class Drawable
